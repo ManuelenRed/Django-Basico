abe503ce52e046e48d410adf5ea9a16b4de6b2e8
Manuel en Red <manuelenred1@gmail.com>
2023-04-19 21:38:31 -0500

    Estructura de la aplicacion con la funcionalidad de votar, aun no cuenta los votos

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hola mundo")
-# Create your views here.
+    latest_question_list = Question.objects.all()
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return render(request, "polls/index.html",context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question": question
+    }
+    return render(request, "polls/detail.html", context)
+
+def vote(request, question_id):
+    return HttpResponse(f"Pagina de votación de la pregunta {question_id}")
